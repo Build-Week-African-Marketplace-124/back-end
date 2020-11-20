f5979de347b17bdf4dc4824d1a004d10bdc48bbb
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-11-19 21:22:12 -0700

    database fixes

--- knexfile.js
+++ knexfile.js
@@ -40,7 +40,7 @@ module.exports = {
   production: {
     client: 'pg',
     useNullAsDefault: true,
-    connection: `${process.env.DB_URI}`,
+    connection: `${process.env.DATABASE_URL}`,
     migrations: {
       directory: './data/migrations',
     },
