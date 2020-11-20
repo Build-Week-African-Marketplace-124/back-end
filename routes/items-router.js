855626a1db9d31701cfca12180b74eb1a3758b8e
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-11-20 12:43:24 -0700

    updated put endpoint message

--- routes/items-router.js
+++ routes/items-router.js
@@ -52,7 +52,7 @@ router.put('/:id', auth(), async (req, res, next) => {
     const itemToUpdate = await db.update(id, item);
 
     if (itemToUpdate) {
-      res.status(200).json(message: "Item " + `${id}` + "was updated");
+      res.status(200).json({message: "Item " + `${id}` + "was updated"});
     } else {
       return res.status(404).json({
         message: 'Item not found.',
